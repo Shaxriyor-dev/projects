6f7f466f3974fddef9f3001043e889eaa609b125
Shaxriyor <shaxriyorturgunboyovshaxriyor@gmail.com>
2024-09-23 21:19:06 +0500

    first commit

---
+++ Welcom/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import w from '../src/assets/w.png'
+import bg from '../src/assets/bg.png'
+function App() {
+
+  return (
+    <>
+       <header>
+             <nav>
+                  <img src={w} alt="" />
+             </nav>
+       </header>
+    </>
+  )
+}
+
+export default App
